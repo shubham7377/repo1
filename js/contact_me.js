c9fa76904162d0fac2bcfde3bf7700f1ff3cad52
shubham7377 <34712802+shubham7377@users.noreply.github.com>
2017-12-30 17:53:43 +0530

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -31,8 +31,6 @@ $(function() {
                     message: message
                 },
                 cache: false,
-                crossDomain: true,
-                dataType: 'jsonp',
                 success: function() {
                     // Enable button & show success message
                     $("#btnSubmit").attr("disabled", false);
